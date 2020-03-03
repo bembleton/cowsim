acb4802b0c3dda25fcffd4a0bdcba360f626e8c3
Business Cat <bembleton@uship.com>
2020-03-03 15:30:17 -0600

    update player animation and state

--- src/animation.js
+++ src/animation.js
@@ -1,24 +1,42 @@
 export default class Animation {
-    constructor ({ update, duration, frameCount }) {
-        this.onUpdate = update;
-        this.duration = duration;
-        this.frameCount = frameCount;
-        this.currentFrame = 0;
-        this.time = 0;
-    }
+  constructor ({ update, duration = 0, frameskip = 0, framecount = 1 }) {
+    this.state = {
+      onUpdate: update,
+      duration,
+      frameskip,
+      framecount,
+      currentFrame: 0,
+      time: 0
+    };
+  }
 
-    update (time) {
-        this.time += time.elapsed;
-        if (this.time >= this.duration) {
-            this.time -= this.duration;
-            if (this.frameCount) {
-              if (this.currentFrame >= this.frameCount) {
-                  this.currentFrame = 0;
-              }
-              this.onUpdate(this.currentFrame++);
-            } else {
-              this.onUpdate();
-            }
-        }
+  update (gametime) {
+    const { duration, frameskip, framecount } = this;
+    let shouldUpdate = false;
+    
+    if (duration) {
+      this.time += gametime.elapsed;
+      if (this.time >= duration) {
+        shouldUpdate = true;
+        this.time -= duration;
+      }
+    } else if (frameskip > 0) {
+      if (time++ >= frameskip) {
+        shouldUpdate = true;
+        this.time = 0;
+      }
     }
+      
+    if (shouldUpdate) {
+      if (this.currentFrame >= framecount) {
+          this.currentFrame = 0;
+      }
+      onUpdate(this.currentFrame++);
+    }
+  }
+
+  reset () {
+    this.time = 0;
+    this.currentFrame = 0;
+  }
 }
--- src/player.js
+++ src/player.js
@@ -1,15 +1,4 @@
-const state = {
-    idle: {
-        update: (player, time) => {
-            
-        }
-    },
-    walking: 'walking',
-    shooting: 'shooting',
-    jumping: 'jumping',
-    dying: 'dying',
-    dead: 'dead',
-};
+
 const dir = {
     up: 'up',
     right: 'right',
@@ -18,17 +7,43 @@ const dir = {
 };
 export default class Player {
     constructor (x, y) {
-        this.state = state.idle;
-        this.x = x;
-        this.y = y;
-        this.dx = 0;
-        this.dy = 0;
-        this.facing = dir.down;
+      this.x = x;
+      this.y = y;
+      this.dx = 0;
+      this.dy = 0;
+      this.facing = dir.down;
+
+      this.states = {
+        idle: new Animation({
+          duration: 500,
+          framecount: 2,
+          update: this.idle
+        }),
+        walking: 'walking',
+        shooting: 'shooting',
+        dying: 'dying',
+        dead: 'dead',
+      };
+
+      this.state = this.states.idle;
     }
 
     update (time) {
-        switch (this.state) {
-            case state.idle:
-        }
+      // get input and update state if necessary
+      // this.setState(this.states.walking)
+      this.state.update(time);
+    }
+
+    idle (frame) {
+
+    }
+
+    isIdle () { this.state === this.states.idle }
+    isWalking () { this.state === this.states.walking }
+    isShooting () { this.state === this.states.shooting }
+
+    setState (newState) {
+      newState.reset();
+      this.state = newState;
     }
 }
