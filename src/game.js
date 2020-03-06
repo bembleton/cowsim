bda9f6391afc63a91a97e6e9de6cce86db3dd4f9
Business Cat <bembleton@uship.com>
2020-03-06 17:47:39 -0600

    refactor title and terrain screens out of game class

--- src/game.js
+++ src/game.js
@@ -1,57 +1,35 @@
 import AnimationFrame from 'animation-frame';
-import { isPressed, buttons } from './controller';
 // import { inputs, isPressed, getAxis } from './gamepad';
 import Display from './display';
 import spriteManager from './spriteManager';
 import ppu from './ppu';
 import loadBitmap from './bitmapLoader';
-import text from './text';
 import gametime from './gametime';
-import Animation from './animation';
-import terrain from './terrain';
 import tileSheet from './assets/background.bmp';
 import spriteSheet from './assets/sprites.bmp';
+import LoadingScreen from './loadingScreen';
+import TerrainScreen from './terrainScreen';
 
 const {
     HORIZONTAL,
-    VERTICAL,
-    setCommonBackground,
     setMirroring,
-    setScroll,
     setNametable,
-    setAttribute,
-    setBgPalette,
-    setSpritePalette,
     setSpriteData,
     setBackgroundData,
     getPixel,
 } = ppu;
 
-const BLACK = 0x3f;
-const WHITE = 0x30;
-
 const BLANK = 0xFF;
-const BRICK = 0x30;
-const CIRCLE_TL = 0x31;
-const CIRCLE_TR = 0x32;
-const CIRCLE_BL = 0x33;
-const CIRCLE_BR = 0x34;
-
-const rand = () => Math.random();
-const randInt = (max) => Math.floor(Math.random() * max);
 
 export default class Game {
     constructor (canvas) {
         this.display = new Display(canvas.getContext('2d'));
-        this.state = {
-            attract: true,
-            menuOpen: false,
-        }
-        this.scroll = {
-            x: 0,
-            y: 0
-        };
         this.animationFrame = new AnimationFrame(60);
+        this.screens = {
+          title: new LoadingScreen(this),
+          terrain: new TerrainScreen(this)
+        };
+        this.currentScreen = {};
     }
     
     async reset () {
@@ -60,8 +38,12 @@ export default class Game {
         await loadBitmap(tileSheet, setBackgroundData);
         await loadBitmap(spriteSheet, setSpriteData);
         spriteManager.clearSprites();
+        this.loadScreen(this.screens.title);
+    }
 
-        this.loadTitleScreen();
+    loadScreen (screen) {
+      this.currentScreen = screen;
+      screen.load();
     }
 
     play () {
@@ -75,16 +57,13 @@ export default class Game {
             const elapsed = timestamp - self.time.timestamp;
             const time = new gametime(timestamp, elapsed);
             self.time = time;
-    
             self.update(time);
             self.draw();
-    
             self.frameId = self.animationFrame.request(tick);
         }
 
         this.frameId = this.animationFrame.request(tick);
     }
-
 
     pause () {
         this.animationFrame.cancel(this.frameId);
@@ -96,139 +75,8 @@ export default class Game {
         if (this.onUpdate) {
             this.onUpdate();
         }
-        // if (this.titleAnimation) {
-        //     this.titleAnimation.update(time);
-        // }
-        if (isPressed(buttons.START)) {
-            terrain.load();
-            this.state.menuOpen = false;
-        }
-
         
-
-        if (this.pacmanAnimation) {
-            this.pacmanAnimation.update(time);
-        }
-
-        const scrollAmt = 1;
-        if (isPressed(buttons.UP)) {
-            this.scroll.y -= scrollAmt;
-        } else if (isPressed(buttons.DOWN)) {
-            this.scroll.y += scrollAmt;
-        }
-        if (isPressed(buttons.RIGHT)) {
-            this.scroll.x += scrollAmt;
-        } else if (isPressed(buttons.LEFT)) {
-            this.scroll.x -= scrollAmt;
-        }
-
-        setScroll(this.scroll.x, this.scroll.y);
-    }
-
-    loadTitleScreen () {
-        setCommonBackground(BLACK);
-        setMirroring(HORIZONTAL);
-        setScroll(0, 0);
-        this.scroll = {
-            x: 0,
-            y: 0
-        };
-        
-        setBgPalette(0, BLACK, 0x00, 0x10, WHITE);
-        setBgPalette(1, BLACK, 0x11, 0x21, 0x31); // blues
-        setBgPalette(2, BLACK, 0x07, 0x17, 0x27); // oranges
-
-        setSpritePalette(0, BLACK, 0x18, 0x28, 0x38); // pacman
-
-        this.clear();
-        //this.updateTitleScreen();
-        // fill the screen with squiggles
-        this.fillWithSquiggles(0,0, 32,60, 2);
-
-        // add the title
-        for (let y=8; y<15; y++)
-        for (let x=8; x<24; x++) {
-            setNametable(x, y, BLANK);
-        }
-        for (let y=5; y<7; y++)
-        for (let x=5; x<12; x++) {
-            setAttribute(x, y, 1);
-        }
-        text(10,10, 'HELLO WORLD!');
-        text(10,12, 'PRESS START ');
-
-        this.pacman = {
-            x: 68,
-            y: 68,
-            frame: 0,
-            spriteId: spriteManager.requestSprite()
-        };
-        
-        this.updatePacman();
-
-        // animate the screen once a second
-        // this.titleAnimation = new Animation({
-        //     duration: 30,
-        //     update: () => this.updateTitleScreen()
-        // });
-
-        this.pacmanAnimation = new Animation({
-            framecount: 12,
-            update: (frame) => this.updatePacman(frame)
-        });
-
-        this.state.menuOpen = true;
-    }
-
-    updateTitleScreen () {
-        setScroll(0, this.scroll);
-        this.scroll += 1;
-    }
-
-    // fills a rectangle with squiggles
-    fillWithSquiggles (fromx, fromy, tox, toy, palette) {
-        // fill the screen with squiggles
-        for (let y=fromy; y<toy; y++)
-        for (let x=fromx; x<tox; x++) {
-            setNametable(x, y, CIRCLE_TL + randInt(2)+2);
-        }
-
-        // fill the attribute table with orange
-        for (let y=(fromy>>1); y<(toy>>1); y++)
-        for (let x=(fromx>>1); x<(tox>>1); x++) {
-            setAttribute(x, y, palette);
-        }
-    }
-
-    updatePacman (frame) {
-        let { x, y, spriteId } = this.pacman;
-        
-        // chomp animation sequence
-        let idx = [0,1,2,3,2,1][(frame>>1) % 6];
-        let flipx = false;
-        let flipy = false;
-
-        // run around the title
-        if (y === 68 && x < 188 - 8) {
-            x += 1;     // go right
-        } else if (y < 100 -8 && x > 68) {
-            idx += 4;
-            y += 1;     // go down
-            flipy = flipx = true;
-        } else if (x > 68) {
-            x -= 1;     // go left
-            flipx = true;
-        } else {
-            idx += 4;
-            y -= 1;     // go up
-        }
-
-        // update sprite
-        spriteManager.setSprite (spriteId, idx, x, y, flipx, flipy, false, 0);
-
-        // update state
-        this.pacman.x = x;
-        this.pacman.y = y;
+        this.currentScreen.update(time);
     }
 
     clear () {
