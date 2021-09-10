b98defd791d065e74b95788b4bbfd90c36a5e69a
Brian Embleton <bembleton@gmail.com>
2021-09-09 20:13:22 -0500

    replace bigint RNG with Squirrel3

--- src/random.js
+++ src/random.js
@@ -1,53 +1,74 @@
-export const rand = () => Math.random();
-export const randInt = (maxOrMin, max) => {
-  if (max !== undefined) {
-    return Math.floor(Math.random() * (max - maxOrMin) + maxOrMin);
-  }
-  return Math.floor(Math.random() * maxOrMin);
-}
-export const randBool = () => Math.random() < 0.5;
-export const choice = (...choices) => {
-  if (choices.length === 1 && Array.isArray(choices[0])) choices = choices[0];
-  const i = Math.floor(Math.random() * choices.length);
-  return choices[i];
-}
+
+
+/**
+ * Math for Game Programmers: Noise-Based RNG
+ * Squirrel Eiserloh
+ */
+const BIT_NOISE1 = 0xB5297A4D;
+const BIT_NOISE2 = 0x68E31DA4;
+const BIT_NOISE3 = 0x1B56C4E9;
+const PRIME1 = 198491317;
+
+// 1D noise (hash) function
+export const Squirrel3 = (position, seed) => {
+  let mangled = position;
+  mangled *= BIT_NOISE1;
+  mangled += seed;
+  mangled = (mangled ^ (mangled >>> 8)) >>> 0;
+  mangled += BIT_NOISE2;
+  mangled = (mangled ^ (mangled << 8)) >>> 0;
+  mangled *= BIT_NOISE3;
+  mangled ^= (mangled >>> 8);
+  return mangled >>> 0;
+};
+
+export const SquirrelNoise2D = (x, y, seed) => {
+  return Squirrel3(x + y*PRIME1, seed);
+};
 
 export class Randy {
-  static C1  = BigInt(0x9E3779B97F4A7C15);  //saves generating BigInt constructor again and again.
-  static C2  = BigInt(0xBF58476D1CE4E5B9);
-  static C3  = BigInt(0x94D049BB133111EB);
-  static FB1 = BigInt(30);                  //shifting
-  static FB2 = BigInt(27);
-  static FB3 = BigInt(31);
-
-  constructor (seed) {
+  constructor (seed = Date.now()) {
     this.reset(seed);
   }
 
   reset(seed) {
-    this.x = BigInt(seed || new Date().getTime());
+    if (seed !== undefined) this.seed = seed;
+    this.position = 0;
   }
 
+  /** Random float between 0 and 1 */
   next() {
     return this.nextInt() / 0xffffffff;
   }
 
+  /** Random boolean */
   nextBool() {
     return (this.nextInt() & 1) === 1;
   }
 
+  /** Random integer */
   nextInt(max) {
-    var z;
-    this.x += Randy.C1;
-    
-    z = this.x;
-    z = z ^ (z >> Randy.FB1) * Randy.C2;
-    z = z ^ (z >> Randy.FB2) * Randy.C3;
-    z = z ^ (z >> Randy.FB3);
-    const n = Number(BigInt.asUintN(32, z));
+    const n = Squirrel3(this.position++, this.seed);
     return max !== undefined ? (n % max) : n;
   }
 }
+
+/** Non-seeded random functions */
+const _randy = new Randy();
+export const rand = () => _randy.next();
+export const randInt = (maxOrMin, max) => {
+  if (max !== undefined) {
+    return (_randy.nextInt() - maxOrMin) % max;
+  }
+  return _randy.nextInt() % maxOrMin;
+}
+export const randBool = () => _randy.nextBool();
+export const choice = (...choices) => {
+  if (choices.length === 1 && Array.isArray(choices[0])) choices = choices[0];
+  const i = _randy.nextInt(choices.length);
+  return choices[i];
+}
+/*******/
 
 export const accumulateChance = (arry) => {
   const count = arry.reduce((a,x) => a + x.rarity, 0);
